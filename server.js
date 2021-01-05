94f13ccbabdd60d9738864cb694aa734886cbcf0
adavidson-smt <alec.davidson@smashmytrash.com>
2021-01-05 16:50:10 -0500

    added middleware

--- server.js
+++ server.js
@@ -63,24 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 // Add headers
-app.use(function(req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'localhost:3001');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(require('./middleware/headers'));
 
 //Use API routes in the App
 app.use('/api', apiRoutes);
