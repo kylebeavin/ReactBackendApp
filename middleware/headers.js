475f2fbe30ad5c791ce7d20e96367bef0ee9c266
adavidson-smt <alec.davidson@smashmytrash.com>
2021-01-05 16:26:11 -0500

    Added headers

---
+++ middleware/headers.js
@@ -0,0 +1,5 @@
+module.exports = (req, res, next) => {
+    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+    next();
+};
