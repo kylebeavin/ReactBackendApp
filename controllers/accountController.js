27a40f35ca8a7f851adf631dc402c59d317d1f2a
adavidson-smt <alec.davidson@smashmytrash.com>
2021-01-12 12:49:25 -0500

    Updated account controller to match Suri's convention.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -27,100 +27,101 @@ exports.view = function(req, res) {
 };
 
 //For creating new account
-exports.add = function(req, res) {
-    var account = new Account();
-    account.group_id = req.body.group_id; // String, required
-    account.name = req.body.name; // String, required
-    account.owner_id = req.body.owner_id; // String, required
-    account.is_active = req.body.is_active; // Bool, default: true
-    account.stage = req.body.stage; // String, required
-    account.address_street = req.body.address_street; // String, required
-    account.address_city = req.body.address_city; // String, required
-    account.address_state = req.body.address_state; // String, required
-    account.address_zip = req.body.address_zip; // String, required
-    account.email = req.body.email; // String, required
-    account.demo = req.body.demo != null ? req.body.demo : null;
-    account.conversion = req.body.conversion != null ? req.body.conversion : null;
-    account.hauling_contract = req.body.hauling_contract != null ? req.body.hauling_contract : null;
-    account.hauling_expiration = req.body.hauling_expiration != null ? req.body.hauling_expiration : null;
-    account.notes = req.body.notes != null ? req.body.notes : null;
-    account.national = req.body.national != null ? req.body.national : null;
-    account.referral = req.body.referral != null ? req.body.referral : null;
-    account.referral_group_id = req.body.referral_group_id != null ? req.body.referral_group_id : null;
+exports.add = async function(req, res) {
+    try {
+        var account = new Account();
+        account.group_id = req.body.group_id; // String, required
+        account.name = req.body.name; // String, required
+        account.owner_id = req.body.owner_id; // String, required
+        account.is_active = req.body.is_active; // Bool, default: true
+        account.stage = req.body.stage; // String, required
+        account.address_street = req.body.address_street; // String, required
+        account.address_city = req.body.address_city; // String, required
+        account.address_state = req.body.address_state; // String, required
+        account.address_zip = req.body.address_zip; // String, required
+        account.email = req.body.email; // String, required
+        account.demo = req.body.demo != null ? req.body.demo : null;
+        account.conversion = req.body.conversion != null ? req.body.conversion : null;
+        account.hauling_contract = req.body.hauling_contract != null ? req.body.hauling_contract : null;
+        account.hauling_expiration = req.body.hauling_expiration != null ? req.body.hauling_expiration : null;
+        account.notes = req.body.notes != null ? req.body.notes : null;
+        account.national = req.body.national != null ? req.body.national : null;
+        account.referral = req.body.referral != null ? req.body.referral : null;
+        account.referral_group_id = req.body.referral_group_id != null ? req.body.referral_group_id : null;
 
-    //Save and check error
-    account.save(function(err) {
-        if (err)
+        //Save and check error
+        let newAccount = await account.save()
+        if (newAccount) {
             res.json({
-                status: "error",
-                status: 304, // 
-                message: err,
-            });
+                status: "success",
+                status: 201,
+                message: "New Account Added!",
 
-        else res.json({
-            status: "success",
-            status: 201,
-            message: "New Account Added!",
-            data: account
-        });
-    });
+            })
+        } else {
+            res.status(304).json({ status: 'something went wrong' })
+        }
+
+    } catch (err) {
+        res.json({ message: err.message })
+    }
+
 };
 
 // Update Account by Object Id
-exports.update = function(req, res) {
-    Account.findById(req.params._id, function(err, account) {
-        if (err)
-            res.json({
-                status: "error",
-                status: 304, // 
-                message: err
-            });
-        account.group_id = req.body.group_id;
-        account.name = req.body.name;
-        account.owner_id = req.body.owner_id;
-        account.is_active = req.body.is_active;
-        account.stage = req.body.stage;
-        account.address_street = req.body.address_street;
-        account.address_city = req.body.address_city;
-        account.address_state = req.body.address_state;
-        account.address_zip = req.body.address_zip;
-        account.email = req.body.email;
-        account.demo = req.body.demo;
-        account.conversion = req.body.conversion;
-        account.hauling_contract = req.body.hauling_contract;
-        account.hauling_expiration = req.body.hauling_expiration;
-        account.notes = req.body.hauling_expiration;
-        account.national = req.body.national;
-        account.referral = req.body.referral;
-        account.referral_group_id = req.body.referral_group_id;
-        //save and check errors
-        account.save(function(err) {
-            if (err)
-                res.json({
-                    status: "error",
-                    status: 304, // 
-                    message: err
-                });
-            else res.json({
-                status: "success",
-                status: 200,
-                message: "Account Updated Successfully",
-                data: account
-            });
-        });
-    });
+exports.update = async function(req, res) {
+    try {
+        let accountToUpdate = await Account.findById(req.params._id).exec()
+
+        if (orderToUpdate) {
+            account.group_id = req.body.group_id;
+            account.name = req.body.name;
+            account.owner_id = req.body.owner_id;
+            account.is_active = req.body.is_active;
+            account.stage = req.body.stage;
+            account.address_street = req.body.address_street;
+            account.address_city = req.body.address_city;
+            account.address_state = req.body.address_state;
+            account.address_zip = req.body.address_zip;
+            account.email = req.body.email;
+            account.demo = req.body.demo;
+            account.conversion = req.body.conversion;
+            account.hauling_contract = req.body.hauling_contract;
+            account.hauling_expiration = req.body.hauling_expiration;
+            account.notes = req.body.hauling_expiration;
+            account.national = req.body.national;
+            account.referral = req.body.referral;
+            account.referral_group_id = req.body.referral_group_id;
+            let updatedAccount = await accountToUpdate.save()
+            if (updatedAccount) {
+                res.status(204).json({
+                    status: "success",
+                    status: 204,
+                    message: "Account Updated Successfully",
+                    data: updatedAccount
+                })
+            } else {
+                res.status(400).json({ message: 'Failed to update', status: 400 })
+            }
+        } else {
+            res.status(400).json({ message: 'Account not found' })
+        }
+    } catch (err) {
+        res.status(400).json({ message: 'Something went wrong' })
+    }
 };
 
 // Delete Account by Object Id
-exports.delete = function(req, res) {
-    Account.deleteOne({
-        _id: req.params._id
-    }, function(err, contact) {
-        if (err)
-            res.send(err)
-        else res.json({
-            status: "success",
-            message: 'Account Deleted by Object Id'
-        });
-    });
+exports.delete = async function(req, res) {
+    try {
+        let deleteAccount = Account.deleteOne({ _id: req.params._id }).exec()
+        if (deleteAccount) {
+            res.status(204).json({ message: 'Account successfully deleted' })
+        } else {
+            res.status(400).json({ message: 'Failed to delete' })
+        }
+    } catch (err) {
+        res.status(400).json({ message: 'Something went wrong' })
+    }
+
 };
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -28,80 +28,74 @@ exports.view = function(req, res) {
 
 //For creating new order
 exports.add = async function(req, res) {
-    try{
-    const order = new Order();
-    order.account_id = req.body.account_id; // String, required
-    order.group_id = req.body.group_id; // String, required
-    order.is_recurring = req.body.is_recurring; // String, required
-    order.service_date = req.body.service_date; // Bool, default: true
-    order.created = req.body.created; // String, required
-    order.is_active = req.body.is_active; // String, required
-    
+    try {
+        const order = new Order();
+        order.account_id = req.body.account_id; // String, required
+        order.group_id = req.body.group_id; // String, required
+        order.is_recurring = req.body.is_recurring; // String, required
+        order.service_date = req.body.service_date; // Bool, default: true
+        order.created = req.body.created; // String, required
+        order.is_active = req.body.is_active; // String, required
 
-    //Save and check error
-    let newOrder = await order.save()
-    if(newOrder){
-        res.json({status: "success",
-        status: 201,
-        message: "New order created!",
-         })
-    }
-    else{
-        res.status(304).json({status:'something went wrong'})
+
+        //Save and check error
+        let newOrder = await order.save()
+        if (newOrder) {
+            res.json({
+                status: "success",
+                status: 201,
+                message: "New order created!",
+            })
+        } else {
+            res.status(304).json({ status: 'something went wrong' })
+        }
+
+    } catch (err) {
+        res.json({ message: err.message })
     }
 
-    }
-    catch(err){
-        res.json({message:err.message})
-    }
-    
 };
 
 // Update order by Object id
 exports.update = async function(req, res) {
-    try{
+    try {
         let orderToUpdate = await Order.findById(req.params._id).exec()
-        if(orderToUpdate){
+        if (orderToUpdate) {
             orderToUpdate.account_id = req.body.account_id
             orderToUpdate.group_id = req.body.group_id
             orderToUpdate.is_recurring = req.body.is_recurring
-           let updatedOrder = await orderToUpdate.save()
-           if(updatedOrder){
-               res.status(204).json({
-                status: "success",
-                status: 204,
-                message: "Order Updated Successfully",
-                data: updatedOrder
-            })
-           }
-           else{
-               res.status(400).json({message:'Failed to update', status:400})
-           }
+            let updatedOrder = await orderToUpdate.save()
+            if (updatedOrder) {
+                res.status(204).json({
+                    status: "success",
+                    status: 204,
+                    message: "Order Updated Successfully",
+                    data: updatedOrder
+                })
+            } else {
+                res.status(400).json({ message: 'Failed to update', status: 400 })
+            }
+        } else {
+            res.status(400).json({ message: 'Order not found' })
         }
-        else{
-            res.status(400).json({message:'Order not found'})
-        }
+    } catch (err) {
+        res.status(400).json({ message: 'Something went wrong' })
     }
-    catch(err){
-        res.status(400).json({message:'Something went wrong'})
-    }
-    
-               
+
+
 };
 
 // Delete Account by Object Id
 exports.delete = async function(req, res) {
-    try{
-        let deleteOrder = Order.deleteOne({_id:req.params._id}).exec()
-        if(deleteOrder){
-            res.status(204).json({message:'Order successfully deleted'})
+    try {
+        let deleteOrder = Order.deleteOne({ _id: req.params._id }).exec()
+        if (deleteOrder) {
+            res.status(204).json({ message: 'Order successfully deleted' })
+        } else {
+            res.status(400).json({ message: 'Failed to delete' })
         }
-        else{
-            res.status(400).json({message:'Failed to delete'})
-        }
+    } catch (err) {
+        res.status(400).json({ message: 'Something went wrong' })
     }
-    catch(err){
-        res.status(400).json({message:'Something went wrong'})
-    }
-    
+
 };
