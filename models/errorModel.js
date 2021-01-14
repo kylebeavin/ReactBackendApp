588bc1ddadf716e681b25fbda3256de6ab9cc41a
adavidson-smt <alec.davidson@smashmytrash.com>
2021-01-14 14:25:30 -0500

    Added error model and controller. They are incomplete and I will get back to those when I finish adding agreement / smash order features.

---
+++ models/errorModel.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+//schema
+var errorSchema = mongoose.Schema({
+    error_message: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    // Corporate, Admin, Partner, Driver, Mechanic, Sales, GM
+    role: {
+        type: String,
+        enum: ['corporate', 'admin', 'partner', 'gm', 'sales', 'driver', 'mechanic'],
+        required: true
+    },
+    // Franchise ID
+    group_id: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    // Mark error active / inactive
+    is_active: {
+        type: Boolean,
+        default: true
+    }
+});
+
+// Export Error Model
+var Error = module.exports = mongoose.model('errors', errorSchema);
+
+module.exports.get = function(callback, limit) {
+    Error.find(callback).limit(limit);
+}
