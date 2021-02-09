2a86cbd283cb01e2db131075093bca0bc1bfb816
suri2021 <suravita.roy@tcmcllc.com>
2021-02-09 08:54:03 -0500

    server

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-
+app.get('/api',(req:Request,res:Response)=>{
+    res.status(200).send('Welcome to the Api')
+})
 app.use('/api', accountRoute);
 app.use('/api', userRoute)
 app.use('/api', agreementRoute )
