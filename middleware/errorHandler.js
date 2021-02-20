eba3d02dbd907b01dba629fed0dcd7f773b2ee83
adavidson-smt <alec.davidson@smashmytrash.com>
2021-02-20 09:08:23 -0500

    This is a bad idea

--- middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production'? 'Sorry, something went wrong': error.stack
-    })
-}
-module.exports = errorHandler
