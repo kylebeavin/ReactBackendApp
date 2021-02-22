740676f54f4614e1299a830284739c02340c850e
adavidson-smt <alec.davidson@smashmytrash.com>
2021-02-22 11:59:05 -0500

    Added route stage to route controller

--- src/interfaces/routesInterface.ts
+++ src/interfaces/routesInterface.ts
@@ -4,6 +4,7 @@ export default interface IRoutes extends Document {
     group_id: string
     truck_id: string
     is_active: boolean
+    route_status: string
     start_location: string
     driver: string
     truck_vin: string
