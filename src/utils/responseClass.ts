72acab307036ba067a99d756f8e5dc913586bf8e
suri2021 <suravita.roy@tcmcllc.com>
2021-02-22 13:45:32 -0500

    response class

--- src/utils/responseClass.ts
+++ src/utils/responseClass.ts
@@ -1,3 +1,23 @@
-class HttpResponse {
-    constructor(message)
+export class HttpResponse {
+    status : number;
+    message:string;
+    type:string;
+    data: string | null
+    constructor(status:number, type:string, message:string, data:string|null){
+        this.status = status
+        this.message = message
+        this.data = JSON.stringify(data)
+        this.type = type
+    }
+    
+    sendResponse(){
+        return {
+            status:this.status,
+            type:this.type,
+            message:this.message,
+            data:this.data
+            
+        }
+    }
+
 }
